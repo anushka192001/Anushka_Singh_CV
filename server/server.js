324f2153dcdf42da6840950eaf70da9d44242ec4
anushka <63954873+anushka192001@users.noreply.github.com>
2025-03-28 21:14:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.post("/submit-form", async (req, res) => {
 });
 
 // ✅ Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
